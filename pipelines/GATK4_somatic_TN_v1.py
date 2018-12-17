802737534a16f0bc164db97a551a29fdf380afb7
ptriska <45852775+ptriska@users.noreply.github.com>
2018-12-17 13:44:32 +0100

    Update GATK4_somatic_TN_v1.py

--- pipelines/GATK4_somatic_TN_v1.py
+++ pipelines/GATK4_somatic_TN_v1.py
@@ -7,22 +7,8 @@ import logging
 import os
 from gatk4_docker import gatk_docker
 from multiprocessing import Process
+from config_file import *
 
-
-### PATH DEFINITIONS #############################################################################
-
-base_folder = "/home/NGS_pipeline/"
-reference_folder = "/home/Reference/"
-output_folder = "/home/output/"
-bwa_index_GRCh38 = reference_folder+"/hg38_gatk/v0/"
-bwa_index_hg19 = reference_folder+"/hg19_old_functional/broad_bundle_hg19_v2.5/"
-reference_fasta_GRCh38 = reference_folder+"/hg38_gatk/v0/Homo_sapiens_assembly38.fasta"
-reference_fasta_hg19 = reference_folder+"/hg19_old_functional/broad_bundle_hg19_v2.5/ucsc.hg19.fasta"
-dbsnp_vcf_GRCh38 = reference_folder+"/GRCh38/hg38_broad_bundle/dbsnp_146.hg38.vcf.gz"
-dbsnp_vcf_hg19 = reference_folder+"/hg19_old_functional/broad_bundle_hg19_v2.5/dbsnp_137.hg19.vcf"
-vep_reference_folder_hg19 = reference_folder+"/hg19_old_functional/"
-
-### END OF PATH DEFINITIONS ######################################################################
 
 def check_path(path):                		## Check if file exists
 		if os.path.exists(path) == True:
