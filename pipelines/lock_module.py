6426dad9ce85463c65efbefb6bcc6899f372ff98
ptriska <45852775+ptriska@users.noreply.github.com>
2019-01-24 14:20:03 +0100

    Update lock_module.py

--- pipelines/lock_module.py
+++ pipelines/lock_module.py
@@ -3,7 +3,7 @@ import os
 import fcntl
 import time
 
-def check_threads(batch_ID, task_ID,start_finish, threads_change, max_threads):
+def check_threads(batch_ID, task_ID,start_finish, threads_change, max_nr_threads):
 	to_lock = open("pipelines/"+batch_ID+".CPUlog",'r+')    
 	fcntl.flock(to_lock, fcntl.LOCK_EX)
 	log=to_lock.read().splitlines()
@@ -12,12 +12,12 @@ def check_threads(batch_ID, task_ID,start_finish, threads_change, max_threads):
 	threads_in_use=lastline.split()[-1]
 	threads_update= int(threads_in_use) + int(threads_change)
 	
-	if threads_update > max_threads:
+	if threads_update > max_nr_threads:
 		fcntl.flock(to_lock, fcntl.LOCK_UN)
 		to_lock.close()
 		return "wait"
 		
-	if int(threads_update) <= int(max_threads):			
+	if int(threads_update) <= int(max_nr_threads):			
 		time_stamp=time.time()
 		to_lock.write(str(time_stamp)+" "+task_ID+" "+start_finish+" "+str(threads_update)+"\n")
 		fcntl.flock(to_lock, fcntl.LOCK_UN)
